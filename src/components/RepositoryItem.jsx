fce3dd32d98863b0bdb99c4875b58af0f8bd1aab
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2023-11-01 20:56:31 +0200

    Exercise 10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import { Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </>
+  );
+};
+
+export default RepositoryItem;
